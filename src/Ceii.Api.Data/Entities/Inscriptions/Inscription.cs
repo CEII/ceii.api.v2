3465cad3e6d1c74e2341210aecd6abbafb321597
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2022-02-01 19:32:12 -0600

    feat: Adding Inscription entity

---
+++ src/Ceii.Api.Data/Entities/Inscriptions/Inscription.cs
@@ -0,0 +1,15 @@
+﻿using Ceii.Api.Data.Entities.Users;
+using Ceii.Api.Data.Entities.Activities;
+
+namespace Ceii.Api.Data.Entities.Inscriptions;
+
+public class Inscription
+{
+    public int Id { get; set; }
+    
+    public List<User>? User { get; set; }
+    
+    public List<Activity>? Activity { get; set; }
+    
+    public DateTime? CreatedAt { get; set; }
+}
