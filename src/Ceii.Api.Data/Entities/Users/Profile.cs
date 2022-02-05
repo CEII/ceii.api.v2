50aacd57ba917fc84d3e33d2a8ca50bac168957d
wmoralesdev <walterrafael26@gmail.com>
2022-02-05 14:46:19 -0600

    refactor: change in format of Profile.cs

--- src/Ceii.Api.Data/Entities/Users/Profile.cs
+++ src/Ceii.Api.Data/Entities/Users/Profile.cs
@@ -1,12 +1,12 @@
-﻿using Ceii.Api.Data.Entities.Users;
-
-namespace Ceii.Api.Data.Entities.New;
+﻿namespace Ceii.Api.Data.Entities.Users;
 
 public class Profile
 {
     public int Id { get; set; }
+    
     public string? Occupation { get; set; }
+    
     public string? Description { get; set; }
+    
     public User? UserEmail { get; set; }
-
 }
