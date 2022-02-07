6a8da391434064f26479b1d5edc3fb1b8af7599c
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2022-02-06 22:48:02 -0600

    feat:Adding Contract, Repository and Services for Inscriptions

---
+++ src/Ceii.Api.Application/Services/Inscriptions/InscriptionService.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Ceii.Api.Application.Repositories;
+
+namespace  Ceii.Api.Application.Services.Inscriptions;
+
+public class InscriptionService
+{
+    private readonly InscriptionRepository _repository;
+    private readonly IMapper _mapper;
+
+    public InscriptionService(InscriptionRepository repository, IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+}
+
--- src/Ceii.Api.Data/Context/ApplicationDbContext.cs
+++ src/Ceii.Api.Data/Context/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using Ceii.Api.Data.Entities.Inscriptions;
 using Ceii.Api.Data.Entities.Users;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +10,8 @@ public class ApplicationDbContext : DbContext
     public DbSet<User>? Users { get; set; }
     
     public DbSet<Role>? Roles { get; set; }
+
+    public DbSet<Inscription>? Inscriptions { get; set; }
 
     public ApplicationDbContext(DbContextOptions options) : base(options)
     {
