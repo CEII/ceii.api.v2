f59a9aaa198cf96fc167697be53a7889336b33e9
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2022-02-07 07:23:10 -0600

    fix: Fixing requested changes

--- src/Ceii.Api.Application/Services/Inscriptions/InscriptionMappings.cs
+++ src/Ceii.Api.Application/Services/Inscriptions/InscriptionMappings.cs
@@ -1,14 +1,7 @@
 ﻿using AutoMapper;
-using Ceii.Api.Application.Services.Inscriptions.ViewModels;
-using Ceii.Api.Data.Entities.Inscriptions;
 
 namespace Ceii.Api.Application.Services.Inscriptions;
 
 public class InscriptionMappings : Profile
 {
-    public InscriptionMappings()
-    {
-        CreateMap<Inscription, InscriptionVm>();
-        CreateMap<InscriptionVm, Inscription>();
-    }
 }
