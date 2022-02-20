c46cb3a93975fe832a85790f9d602a135f90e4e8
MarianaLagrava <marianalagrava@gmail.com>
2022-02-19 23:16:53 -0600

    feat: Added Inscriptions ViewModel

---
+++ src/Ceii.Api.Application/Services/Inscriptions/ViewModels/InscriptionVm.cs
@@ -0,0 +1,13 @@
+﻿
+using Ceii.Api.Application.Services.Users.ViewModels;
+
+namespace Ceii.Api.Application.Services.Inscriptions.ViewModels;
+
+public class InscriptionVm
+{
+    public UserVm? User { get; set; }
+    
+    // public List<Activity>? Activity { get; set; }
+    
+    public DateTime? CreatedAt { get; set; }
+}
---
+++ src/Ceii.Api.Core/Controllers/InscriptionController.cs
@@ -0,0 +1,24 @@
+﻿using Ceii.Api.Application.Services.Developers;
+using Ceii.Api.Application.Services.Inscriptions.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ceii.Api.Core.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DeveloperController : ControllerBase
+{
+    private readonly DevelopersServices _service;
+
+    public DeveloperController(DevelopersServices service)
+    {
+        _service = service;
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult> Delete(string id)
+    {
+        var dev = await _service.Delete(id);
+        return dev is null ? NotFound() : Ok();
+    }
+}
