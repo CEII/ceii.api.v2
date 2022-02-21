b02869d078c095b2d25e9c6f97a62eea2e840af6
MarianaLagrava <marianalagrava@gmail.com>
2022-02-20 21:38:04 -0600

    feat: Dependency Injection and Controller

---
+++ src/Ceii.Api.Core/Controllers/CourseController.cs
@@ -0,0 +1,19 @@
+﻿using Ceii.Api.Application.Services.Courses;
+using Ceii.Api.Application.Services.Courses.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ceii.Api.Core.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+
+public class CourseController:ControllerBase
+{
+    private readonly CourseService _service;
+
+    [HttpGet]
+    public async Task<ActionResult<CourseVm>> GetCourseById(int id)
+    {
+        var course = await _service.GetById(id);
+        return course is null ? NotFound() : Ok(course);
+    }
+}
